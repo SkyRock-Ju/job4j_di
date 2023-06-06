49a0fd2e6b7308f186ed30be9278c4acc056e90a
Dulustan <skyrockpk@gmail.com>
2023-06-06 14:38:09 +0700

    dependency injection with spring context

---
+++ src/main/java/SpringDI.java
@@ -0,0 +1,18 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import repository.Store;
+import service.StartUI;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(StartUI.class);
+        context.refresh();
+
+        StartUI ui = context.getBean(StartUI.class);
+
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
+    }
+}
