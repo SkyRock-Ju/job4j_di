2d917ed2434423f26e825cf1d85c4de785b05842
Dulustan <skyrockpk@gmail.com>
2023-06-06 15:34:59 +0700

    refactored StartUI, deleted constructor and added @Autowired

--- src/main/java/ru/job4j/di/service/StartUI.java
+++ src/main/java/ru/job4j/di/service/StartUI.java
@@ -1,16 +1,14 @@
 package ru.job4j.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.job4j.di.repository.Store;
 
 @Component
 public class StartUI {
 
+    @Autowired
     private Store store;
-
-    public StartUI(Store store) {
-        this.store = store;
-    }
 
     public void add(String value) {
         store.add(value);
